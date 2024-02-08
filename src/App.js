7f2e92995876dbf37d5165665bf780b271b1479b
puru888 <papujara162@gmail.com>
2024-02-08 07:04:56 -0500

    verify state functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  constructor(){
+    super();
+    
+    this.state = {name : 'world'};
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {this.state.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+        onClick={()=>{
+            this.setState({name : 'purav'});
+            }
+        }
         >
-          Learn React
-        </a>
+          change name
+        </button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
